c5f99141cd044e7234124438d9983ba8f6c4417a
majose-2023 <144177475+majose-2023@users.noreply.github.com>
2023-09-10 08:52:35 -0400

    commiting

---
+++ script.js
@@ -0,0 +1,23 @@
+//console.log("Hola Mundo");
+//let miVariable = 123;
+//miVariable = 321;
+//const MY_VARIABLE = 123;
+
+let num1 = 5;
+let num2 = 6;
+let num3 = 10;
+
+let mayor;
+if (num1 > num2 ) {
+  mayor=num1;
+  console.log("El mayos es:"+mayor)
+} else { 
+    if(num2>num3){
+    mayor=num2;
+    console.log("El mayos es:"+mayor)
+    } else {
+    mayor=num3;
+    console.log("El mayos es:"+mayor)
+    }
+   
+}
